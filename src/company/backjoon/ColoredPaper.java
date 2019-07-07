660f3bee11fbd039e8c686f9f8ca72a8a58a5d8a
doyoung Hwang <dodo4513@nhnent.com>
2019-07-07 15:40:09 +0900

    알고리즘!

--- src/company/backjoon/ColoredPaper.java
+++ src/company/backjoon/ColoredPaper.java
@@ -56,40 +56,6 @@ public class ColoredPaper {
 
     System.out.println(whiteResult);
     System.out.println(blackResult);
-
-    return;
   }
 }
 
-/*
-
-8
-    1 1 0 0 0 0 1 1
-    1 1 0 0 0 0 1 1
-    0 0 0 0 1 1 0 0
-    0 0 0 0 1 1 0 0
-
-    1 0 0 0 1 1 1 1
-    0 1 0 0 1 1 1 1
-    0 0 1 1 1 1 1 1
-    0 0 1 1 1 1 1 1
-
-4
-0 0 0 0
-0 0 1 1
-0 0 1 1
-0 0 1 1
-
-
-4
-1 1 1 1
-1 1 1 1
-1 1 1 1
-1 1 1 1
-
-
-2
-1 1
-1 0
-
-*/
